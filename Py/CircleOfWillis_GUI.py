152e3f32fe9a96d7eb0db1c03b0df24cd3ce1f92
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2022-12-13 10:12:37 +0000

    Update CircleOfWillis_GUI.py

--- Py/CircleOfWillis_GUI.py
+++ Py/CircleOfWillis_GUI.py
@@ -25,15 +25,32 @@ def button_up():
     
     first_number = int(e.get())
     if first_number>= (slices-1):
-        first_number = slices-1
+        first_number = slices-2
     e.delete(0,END)
     e.insert(0,1+(first_number))
 
     
     slice_2 = data[:, :, int(e.get())]
-    myImage       = ImageTk.PhotoImage(image=Image.fromarray(slice_2))
+    myImage       = ImageTk.PhotoImage(image=Image.fromarray(slice_2).resize((320,320)))
     myLabel       = Label(image=myImage)
-    myLabel.grid(row=0, column=1,columnspan=5)
+    myLabel.grid(row=0, column=1,columnspan=7)
+    
+def button_up_double():
+    global myImage   
+    global myLabel     
+    myLabel.grid_forget()
+    
+    first_number = int(e.get())
+    if first_number>= (slices-4):
+        first_number = slices-6
+    e.delete(0,END)
+    e.insert(0,5+(first_number))
+
+    
+    slice_2 = data[:, :, int(e.get())]
+    myImage       = ImageTk.PhotoImage(image=Image.fromarray(slice_2).resize((320,320)))
+    myLabel       = Label(image=myImage)
+    myLabel.grid(row=0, column=1,columnspan=7)
 
 def changeSlice():
     return
@@ -49,11 +66,24 @@ def button_down():
     e.delete(0,END)
     e.insert(0,-1+(first_number))
     slice_2 = data[:, :, int(e.get())]
-    myImage       = ImageTk.PhotoImage(image=Image.fromarray(slice_2))
+    myImage       = ImageTk.PhotoImage(image=Image.fromarray(slice_2).resize((320,320)))
     myLabel       = Label(image=myImage)
-    myLabel.grid(row=0, column=1,columnspan=5)
+    myLabel.grid(row=0, column=1,columnspan=7)
 
+def button_down_double():
+    global myImage   
+    global myLabel     
+    myLabel.grid_forget()
 
+    first_number = int(e.get())
+    if first_number<= 4:
+        first_number = 5
+    e.delete(0,END)
+    e.insert(0,-5+(first_number))
+    slice_2 = data[:, :, int(e.get())]
+    myImage       = ImageTk.PhotoImage(image=Image.fromarray(slice_2).resize((320,320)))
+    myLabel       = Label(image=myImage)
+    myLabel.grid(row=0, column=1,columnspan=7)
 root = Tk()
 root.title("Circle of Willis")
 root.iconbitmap('CircleW.ico')
@@ -71,23 +101,51 @@ slices   = dims[2]
 #print(data[0:2,0:2,0])
 
 
+Ax_MIP = np.max(data, axis=2)
+Sag_MIP= np.max(data, axis=1)
+Cor_MIP= np.max(data, axis=0)
+
+Sag_MIP = np.rot90(Sag_MIP, k=3, axes=(1,0))
+Cor_MIP = np.rot90(Cor_MIP, k=3, axes=(1,0))
+
+
+#print(type(q))
 slice_2 = data[:, :, int(e.get())]
-print(type(slice_2))
+
 #plt.imshow(slice_2)
 #myImage = ImageTk.PhotoImage(Image.open("Circle_of_Willis.png"))
-myImage       = ImageTk.PhotoImage(image=Image.fromarray(slice_2))
+myImage       = ImageTk.PhotoImage(image=Image.fromarray(slice_2).resize((320,320)))
 myLabel       = Label(image=myImage)
 
-button_up     = Button (root, text = ">>",padx=42,pady=20, command=button_up)
-button_down   = Button (root, text = "<<",padx=40,pady=20, command=button_down)
+Ax_MIP_image  = ImageTk.PhotoImage(image=Image.fromarray(Ax_MIP).resize((320,320)))
+Ax_MIP_Label  = Label(image=Ax_MIP_image)
+Sag_MIP_image  = ImageTk.PhotoImage(image=Image.fromarray(Sag_MIP).resize((320,120)))
+Sag_MIP_Label  = Label(image=Sag_MIP_image)
+Cor_MIP_image  = ImageTk.PhotoImage(image=Image.fromarray(Cor_MIP).resize((320,120)))
+Cor_MIP_Label  = Label(image=Cor_MIP_image)
+
+button_up            = Button (root, text = ">",padx=10,pady=10, command=button_up)
+button_down          = Button (root, text = "<",padx=10,pady=10, command=button_down)
+button_up_double     = Button (root, text = ">>",padx=10,pady=10, command=button_up_double)
+button_down_double   = Button (root, text = "<<",padx=10,pady=10, command=button_down_double)
 
 
-myLabel.grid(row=0, column=1,columnspan=5)
-button_down.grid(row=1, column=1)
-button_up.grid(row=1, column=5)
-e.grid(row = 1, column =3, padx = 10, pady= 10)
-minLabel = Label(root, text="0").grid(row = 1, column=2)
-maxLabel = Label(root, text=slices).grid(row = 1, column=4)
+myLabel.grid(row=0, column=1,columnspan=7)
+Ax_MIP_Label.grid(row=2, column=1,columnspan=7) 
+Sag_MIP_Label.grid(row=2, column=8,columnspan=1) 
+Cor_MIP_Label.grid(row=2, column=9,columnspan=1) 
+
+button_down_double.grid(row=1, column=1)
+button_down.grid(row=1, column=2)
+button_up.grid(row=1, column=6)
+button_up_double.grid(row=1, column=7)
+
+e.grid(row = 1, column =4, padx = 10, pady= 10)
+minLabel = Label(root, text="0").grid(row = 1, column=3)
+maxLabel = Label(root, text=slices-1).grid(row = 1, column=5)
+
+
+
 #myImage = ImageTk.PhotoImage(slice_2)
 #myLabel.pack()
 #button_down.pack()
